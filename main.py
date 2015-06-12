c7e0f731922d828b9712fb9f0258e0203860b199
Emmanuel Prochasson <eprochasson@gmail.com>
2015-06-12 16:23:41 +0800

    REF: Output format

--- main.py
+++ main.py
@@ -30,4 +30,4 @@ def load_cases(input_file = 'input.txt'):
 
 cases = load_cases('input.txt')
 for idx, c in enumerate(cases):
-    print "Case {}: {} ({} iterations)".format(idx+1, c.solve(), c.total_iteration)
+    print "Case {}: {}".format(idx+1, c.solve())
